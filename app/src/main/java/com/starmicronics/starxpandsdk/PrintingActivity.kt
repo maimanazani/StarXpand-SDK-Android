c9a6c37d4d098c10be08e8e9334b800062be6ae4
Aiman Azani <m.aimanazani@gmail.com>
2024-09-23 09:26:56 +0800

    wip

--- app/src/main/java/com/starmicronics/starxpandsdk/PrintingActivity.kt
+++ app/src/main/java/com/starmicronics/starxpandsdk/PrintingActivity.kt
@@ -100,72 +100,8 @@ class PrintingActivity : AppCompatActivity() {
                                             "\n"
                                 )
                                 .styleAlignment(Alignment.Left)
-                                .actionPrintText(
-                                    "Date:MM/DD/YYYY    Time:HH:MM PM\n" +
-                                            "--------------------------------\n" +
-                                            "\n"
-                                )
-                                .add(
-                                     PrinterBuilder()
-                                         .styleBold(true)
-                                         .actionPrintText("SALE\n")
-                                )
-                                .actionPrintText(
-                                    "SKU         Description    Total\n" +
-                                            "300678566   PLAIN T-SHIRT  10.99\n" +
-                                            "300692003   BLACK DENIM    29.99\n" +
-                                            "300651148   BLUE DENIM     29.99\n" +
-                                            "300642980   STRIPED DRESS  49.99\n" +
-                                            "300638471   BLACK BOOTS    35.99\n" +
-                                            "\n" +
-                                            "Subtotal                  156.95\n" +
-                                            "Tax                         0.00\n" +
-                                            "--------------------------------\n"
-                                )
-                                .actionPrintText("Total     ")
-                                .add(
-                                    PrinterBuilder()
-                                        .styleMagnification(MagnificationParameter(2, 2))
-                                        .actionPrintText("   $156.95\n")
-                                )
-                                .actionPrintText(
-                                    "--------------------------------\n" +
-                                            "\n" +
-                                            "Charge\n" +
-                                            "156.95\n" +
-                                            "Visa XXXX-XXXX-XXXX-0123\n" +
-                                            "\n"
-                                )
-                                .add(
-                                    PrinterBuilder()
-                                        .styleInvert(true)
-                                        .actionPrintText("Refunds and Exchanges\n")
-                                )
-                                .actionPrintText("Within ")
-                                .add(
-                                     PrinterBuilder()
-                                        .styleUnderLine(true)
-                                        .actionPrintText("30 days")
-                                )
-                                .actionPrintText(" with receipt\n")
-                                .actionPrintText(
-                                    "And tags attached\n" +
-                                            "\n"
-                                )
-                                .styleAlignment(Alignment.Center)
-                                .actionPrintBarcode(
-                                    BarcodeParameter("0123456", BarcodeSymbology.Jan8)
-                                        .setBarDots(3)
-                                        .setHeight(5.0)
-                                        .setPrintHri(true)
-                                )
-                                .actionFeedLine(1)
-                                .actionPrintQRCode(
-                                    QRCodeParameter("Hello, World\n")
-                                        .setLevel(QRCodeLevel.L)
-                                        .setCellSize(8)
-                                )
-                                .actionCut(CutType.Partial)
+                                .actionPrintText("        ")
+                                .actionCut(CutType.TearOff)
                         )
                 )
                 val commands = builder.getCommands()

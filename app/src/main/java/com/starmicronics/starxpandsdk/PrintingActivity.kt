39facf4083eda6f2d1e4d61fd11a1e728c9293a5
Aiman Azani <m.aimanazani@gmail.com>
2024-10-01 06:26:54 +0800

    Update PrintingActivity.kt

--- app/src/main/java/com/starmicronics/starxpandsdk/PrintingActivity.kt
+++ app/src/main/java/com/starmicronics/starxpandsdk/PrintingActivity.kt
@@ -128,9 +128,9 @@ class PrintingActivity : AppCompatActivity() {
                                                 "Date:MM/DD/YYYY Time:HH:MM PM\n" +
                                                 "------------------------------------------------\n" +
                                                 "\n" +
-                                                "SKU       Description   Total\n" +
-                                                "300678566 PLAIN T-SHIRT 10.99\n" +
-                                                "300692003 BLACK DENIM   29.99\n" +
+                                                "SKU                   Description   Total\n" +
+                                                "300678566             PLAIN T-SHIRT 10.99\n" +
+                                                "300692003             BLACK DENIM   29.99\n" +
                                                 "300651148 BLUE DENIM    29.99\n" +
                                                 "300642980 STRIPED DRESS 49.99\n" +
                                                 "300638471 BLACK BOOTS   35.99\n" +
